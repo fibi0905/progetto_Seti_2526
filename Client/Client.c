ba826b2584922281e8ec3d1b677038ec0c33fd82
FerraD05 <samueledavide0903@gmail.com>
2025-12-17 10:17:15 +0100

    modifica menu

--- Client/Client.c
+++ Client/Client.c
@@ -11,8 +11,11 @@
 
 #include "Client.h"
 
-void stampaMenu(){
-    printf("\ninserisci un numero corrispondente alla funzionalità\n1 registrarsi\n2 accedere\n3 inviare una richiesta d'amicizia\n4 inviare un messaggio\n5 invia un messaggio alla tua rete\n6 visiona gli utenti attivi\n7 visiona le tue notifiche\n8 esci\n");
+void stampaMenu(bool sePrimaVolta){
+    if(sePrimaVolta)
+        printf("\ninserisci un numero corrispondente alla funzionalità\n1 registrarsi\n2 accedere\n8 esci\n");
+    else
+        printf("\n3 inviare una richiesta d'amicizia\n4 inviare un messaggio\n5 invia un messaggio alla tua rete\n6 visiona gli utenti attivi\n7 visiona le tue notifiche\n8 esci\n");
 }
 
 int main(const int argc, const char* args[])
@@ -54,10 +57,12 @@ int main(const int argc, const char* args[])
     
     printf("Avvio del client riuscito\n");
     uint8_t sceltaUtente;
+    bool primaVolta = true;
 
     while(true)
     {
-        stampaMenu();
+        bool errprimoAccesso;
+        stampaMenu( (primaVolta || errprimoAccesso) );
         scanf("%hhd", &sceltaUtente);
 
         switch (sceltaUtente)
@@ -65,8 +70,10 @@ int main(const int argc, const char* args[])
         case 1 :
             
             err = newClient();
-            if(err != OK)
+            if(err != OK){
                 printf("registrazione fallita\n");
+                errprimoAccesso = true;
+            }
             else
                 printf("registrazione riuscita\n");
             break;
@@ -74,8 +81,10 @@ int main(const int argc, const char* args[])
         case 2 :
 
             err = login();
-            if(err != OK)
+            if(err != OK){
                 printf("accesso fallito\n");
+                errprimoAccesso = true;
+            }
             else
                 printf("accesso riuscito\n");
             break;
@@ -103,5 +112,7 @@ int main(const int argc, const char* args[])
             
             break;
         }
+
+        primaVolta = false;
     }
 }
